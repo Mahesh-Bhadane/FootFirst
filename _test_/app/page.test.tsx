e435b7b50220d0b299d94703fe010229924c82b6
Mahesh Bhadane <108877616+MaheshBhadane@users.noreply.github.com>
2023-08-25 12:30:45 +0530

    INTG:Jest integrated and created test case for home page

---
+++ _test_/app/page.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Home from "@/app/page";
+import "@testing-library/jest-dom";
+
+describe("Home component", () => {
+  it("Should renders welcome message and button", () => {
+    render(<Home />);
+    
+    expect(screen.getByText("welcome to next.js")).toBeInTheDocument();
+    expect(screen.getByText("Test😊")).toBeInTheDocument();
+  });
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <>
+      <h1 className="text-3xl font-bold underline">welcome to next.js</h1>
       <Button>Test😊</Button>
-    </h1>
+    </>
   );
 }
