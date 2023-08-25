af3e2f236a39069cea482404389969bb12a8d216
Mahesh Bhadane <108877616+MaheshBhadane@users.noreply.github.com>
2023-08-25 18:10:46 +0530

    INTG:Basic folder structure added

---
+++ src/app/error.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="flex items-center justify-center">
+      <h2>Something went wrong!</h2>
+      <Button onClick={() => reset()}>Try again</Button>
+    </div>
+  );
+}
